664b7f4cdb638d67a28fc78f80c9e1dcef2867c0
shot-codes <nicolai.verbaarschot@gmail.com>
2023-11-23 23:04:56 +0100

    Removed hyprland workspace animations.

--- hypr/scripts/toggle-system-theme.sh
+++ hypr/scripts/toggle-system-theme.sh
@@ -5,9 +5,11 @@ if [ "$PREFERS" == "prefer-light" ]; then
 	kitten themes --reload-in=all Alabaster Dark Custom
 	gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 	for f in /run/user/1000/nvim.*; do nvim --server "$f" --headless --remote-expr 'execute("set background=dark")' &>/dev/null; done
+
 else
 	echo "font_family Iosevka Bold" >/home/shot/.config/kitty/font.conf
 	kitten themes --reload-in=all Alabaster Custom
 	gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
 	for f in /run/user/1000/nvim.*; do nvim --server "$f" --headless --remote-expr 'execute("set background=light")' &>/dev/null; done
+
 fi
