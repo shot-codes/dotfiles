563b21acde2ef342dc0fda1f3310976dae42be18
shot-codes <nicolai.verbaarschot@gmail.com>
2024-02-03 17:20:58 +0100

    boinked my bootloader, saving config incase I can't recover.

--- hypr/scripts/toggle-system-theme.sh
+++ hypr/scripts/toggle-system-theme.sh
@@ -1,15 +1,17 @@
 PREFERS=$(gsettings get org.gnome.desktop.interface color-scheme | tr -d "'")
 
 if [ "$PREFERS" == "prefer-light" ]; then
+	# hyprctl keyword general:col.active_border "rgba(ffa100ff) rgba(ff2a00ff) 45deg"
 	echo "font_family Iosevka" >/home/shot/.config/kitty/font.conf
-	kitten themes --reload-in=all Alabaster Dark Custom
+	kitten themes --reload-in=all --cache-age=-1 Alabaster Dark Custom
 	gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 	gsettings set org.gnome.desktop.interface gtk-theme "Materia-dark-compact"
 	for f in /run/user/1000/nvim.*; do nvim --server "$f" --headless --remote-expr 'execute("set background=dark")' &>/dev/null; done
 
 else
+	# hyprctl keyword general:col.active_border "rgba(33ccffff) rgba(00ff99ff) 45deg"
 	echo "font_family Iosevka Bold" >/home/shot/.config/kitty/font.conf
-	kitten themes --reload-in=all Alabaster Custom
+	kitten themes --reload-in=all --cache-age=-1 Alabaster Custom
 	gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
 	gsettings set org.gnome.desktop.interface gtk-theme "Materia-light-compact"
 	for f in /run/user/1000/nvim.*; do nvim --server "$f" --headless --remote-expr 'execute("set background=light")' &>/dev/null; done
--- hypr/scripts/toggle_gaps/toggle_gaps.sh
+++ hypr/scripts/toggle_gaps/toggle_gaps.sh
@@ -3,15 +3,27 @@ STATE_FILE=/home/shot/.config/hypr/scripts/toggle_gaps/gaps_state.txt
 CURRENT_STATE=$(cat "$STATE_FILE")
 
 if [ "$CURRENT_STATE" == "enabled" ]; then
+	# hyprctl dispatch exec pkill waybar
+	swww img ~/Documents/images/wallpapers/black.png --transition-step 15 --transition-fps 120
 	hyprctl keyword general:gaps_in 0
 	hyprctl keyword general:gaps_out 0
-	hyprctl keyword general:col.inactive_border "rgba(000030ff)"
+	hyprctl keyword general:col.inactive_border "rgba(202020ff)"
 	hyprctl keyword decoration:rounding 0
+	hyprctl keyword decoration:active_opacity 1.0
+	hyprctl keyword decoration:inactive_opacity 1.0
+	hyprctl keyword decoration:blur:enabled false
+	hyprctl keyword decoration:drop_shadow no
 	echo "disabled" >"$STATE_FILE"
 else
+	# hyprctl dispatch exec waybar
 	hyprctl keyword general:gaps_in 10
 	hyprctl keyword general:gaps_out 50
 	hyprctl keyword decoration:rounding 5
 	hyprctl keyword general:col.inactive_border "rgba(00000000)"
+	hyprctl keyword decoration:active_opacity 0.9
+	hyprctl keyword decoration:inactive_opacity 0.7
+	hyprctl keyword decoration:blur:enabled true
+	hyprctl keyword decoration:drop_shadow yes
 	echo "enabled" >"$STATE_FILE"
+	swww img ~/Documents/images/wallpapers/1.jpg --transition-step 1 --transition-fps 120
 fi
